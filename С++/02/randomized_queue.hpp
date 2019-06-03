7526f62898994675691275fe73197e7ce9bc5f7a
vladrus13 <vladrus13rus@yandex.ru>
2019-06-03 20:07:42 +0300

    Add 02 C++

---
+++ С++/02/randomized_queue.hpp
@@ -0,0 +1,60 @@
+#include <cstddef>
+#include "RandomizeQueue.h"
+
+template <typename T>
+class randomized_queue {
+private:
+	node<T> *first = nullptr, *last = nullptr; // ends of deque
+	size_t si = 0;
+
+	uint64_t get_rand() const;
+
+	node<T> * move(node<T> * first, int64_t dist) const;
+
+	void inc_size();
+	void dec_size();
+public:
+	
+	class iterator {
+	private:
+		std::vector <T> q;
+		size_t index = 0;
+	public:
+		iterator(node<T> * begin, node<T> * end, int pos);
+
+		bool operator != (iterator const& other) const;
+		bool operator== (iterator const& other) const;
+
+		iterator& operator++(void) {
+			index++;
+			return *this;
+		}
+
+		iterator operator++(int) {
+			iterator result = *this;
+			++*this;
+			return result;
+		}
+
+		T& operator*() const {
+			return q[index];
+		}
+	};
+
+	randomized_queue(T x);
+
+	~randomized_queue();
+
+	randomized_queue();
+	// post: R = size
+	uint64_t size() const;
+
+	// post: R = (empty or not)
+	bool empty() const;
+	void enqueue(T x);
+	T dequeue();
+	T sample() const;
+
+	typename randomized_queue <T>::iterator end();
+	typename randomized_queue <T>::iterator begin();
+};
